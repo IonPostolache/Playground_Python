1cdd748bcb85d0f530697162161ff7ee0e855b8c
ionpostolache <ion_postolache00@yahoo.com>
2021-05-29 11:24:58 +0300

    working

--- no_3_times.py
+++ no_3_times.py
@@ -3,16 +3,11 @@ where every element appears three times except for one.
 Input : [5, 3, 4, 3, 5, 5, 3]
 Output : 4"""
 
+input=[5,3,4,3,5,5,3]
+print(f"Input: {input}")
 
-inputlist=[5, 3, 4, 3, 5, 5, 3]
-print(f"Input: {inputlist}")
-#uniquelist=[]
-
-for i in inputlist:
-    counter=inputlist.count(i)
-    #print(counter)
+for i in input:
+    counter=input.count(i)
     if counter==1:
-        uniquelist.append(i)
         print(i)
 
-#print(uniquelist)
