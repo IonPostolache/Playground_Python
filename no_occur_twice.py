04223bf1525b7d75d6a2e8dbb28878bf14723007
ionpostolache <ion_postolache00@yahoo.com>
2021-05-29 11:15:10 +0300

    working

--- no_occur_twice.py
+++ no_occur_twice.py
@@ -2,49 +2,15 @@
 Input : [5, 3, 4, 3, 4]
 Output : 5   """
 
-
 inputlist=[5, 3, 4, 3, 4]
-print(f"The input list is: {inputlist}")
-resultlist=[]
-"""
+print(f"Input: {inputlist}")
+#uniquelist=[]
+
 for i in inputlist:
-    #print(i)
-    indx=inputlist.index(i)
-    print(id(inputlist[indx]))
-    popul=inputlist.pop(indx)
-    print(popul)
-    #print(indx)
-    #bbb=inputlist[indx]
-    #print(bbb)
-    #popul=inputlist.pop(indx)
-    #print(popul)
-    #bbb=inputlist[indx]
-    #print(bbb)
-    resultlist.append(popul)
-    #print(i)
-    #i=i+1
+    counter=inputlist.count(i)
+    #print(counter)
+    if counter==1:
+        uniquelist.append(i)
+        print(i)
 
-print(resultlist)
-print(inputlist)
-
-for j in inputlist:
-    inputlist.pop()
-    print(inputlist)
-
-print(inputlist)
-"""
-for i in range(0,len(inputlist)):
-    #print(i)
-    each_no=inputlist[i]
-    print(each_no)
-    current_no=inputlist.pop(i)
-    print(current_no)
-    print(inputlist)
-    if current_no in inputlist:
-        print(f"{current_no} is twice")
-        continue
-    else:
-        resultlist.append(current_no)
-
-print(inputlist)
-print(f" these numbers are only once: {resultlist}")
+#print(uniquelist)
