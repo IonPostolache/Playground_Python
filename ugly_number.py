165ad9beb0b5197721e1836ce8e06ffdbf00d466
IonPostolache <ion_postolache00@yahoo.com>
2022-04-30 10:16:37 +0300

    code improved but not finished

--- ugly_number.py
+++ ugly_number.py
@@ -9,10 +9,32 @@ Note: 1 is typically treated as an ugly number
 24.1. Given a number n, the task is to find n’th Ugly number."""
 
 number=int(input("what is the number: > "))
-print(number)
+#print(number)
+prime_factors=[2,3,5]
 
-if number%2==0:
-    result=number/2
-    print(result)
-    if result%2==0:
-        continue
+def divide(n_check, primef):
+    result=n_check/primef
+    print(f"the number {n_check} was divided by {primef} and the result is {result}")
+    return result
+
+for i in prime_factors:
+    if number%i==0:
+        extract=divide(number,i)
+        print(extract)
+        print(type(extract))
+        #the next if needs to be in a while loop to run as many times as neccesary
+        #after that we need to check the other factor
+        #if number/extract in prime_factors:
+            #print("the number is ugly")----this should be the final decision
+        if extract%i==0:
+            extract2=divide(extract,i)
+            print(extract2)
+            if extract2!=2 and extract2!=3 and extract2!=5:
+                print("the number is not ugly")
+            else:
+                print("the number is ugly")
+        else:
+            print(f"the number is not ugly")
+    else:
+        print(f"the number is not ugly")
+
