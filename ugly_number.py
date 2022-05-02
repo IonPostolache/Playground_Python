c3900ec2148022755acad0d9da5c252b51d85262
IonPostolache <ion_postolache00@yahoo.com>
2022-05-02 21:23:31 +0300

    first task solved

--- ugly_number.py
+++ ugly_number.py
@@ -8,32 +8,37 @@ The number 14 is not ugly because in its prime factor the 7 will come.
 Note: 1 is typically treated as an ugly number
 24.1. Given a number n, the task is to find n’th Ugly number."""
 
-number=int(input("what is the number: > "))
+number=int(input("what is the number: "))
 prime_factors=[2,3,5]
 
-def divide(n_check, primef):
-    result=n_check/primef
-    print(f"the number {n_check} was divided by {primef} and the result is {result}")
-    return result
-
-for i in prime_factors:
-    if number%i==0:
-        extract=divide(number,i)
-        print(extract)
-        print(type(extract))
-        #the next if needs to be in a while loop to run as many times as neccesary
-        #after that we need to check the other factor
-        #if number/extract in prime_factors:
-            #print("the number is ugly")----this should be the final decision
-        if extract%i==0:
-            extract2=divide(extract,i)
-            print(extract2)
-            if extract2!=2 and extract2!=3 and extract2!=5:
-                print("the number is not ugly")
-            else:
-                print("the number is ugly")
+def main():
+    number1=number
+    if number == 1 or number in prime_factors:
+        print(1 == True)
+        return
+    flag=True
+    while flag:
+        if number1%2==0:
+            result = number1 / 2
+            if result in prime_factors:
+                print(result in prime_factors)
+                return
+            number1=result
+        elif number1 % 3 == 0:
+            result = number1 / 3
+            if result in prime_factors:
+                print(result in prime_factors)
+                return
+            number1=result
+        elif number1%5==0:
+            result=number1/5
+            if result in prime_factors:
+                print(result in prime_factors)
+                return
+            number1=result
         else:
-            print(f"the number is not ugly")
-    else:
-        print(f"the number is not ugly")
+            print(number1 in prime_factors)
+            return
+            #flag=False
 
+main()
